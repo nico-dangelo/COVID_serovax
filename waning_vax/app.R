6a3a110e6452e99275d485c51a6ac6df21fdcfeb
nico-dangelo <81781015+nico-dangelo@users.noreply.github.com>
2024-02-14 13:43:46 -0500

    Updated Shiny app script

--- waning_vax/app.R
+++ waning_vax/app.R
@@ -1,48 +1,70 @@
 
+
 #
 
 library(shiny)
 
 # Define UI for application that draws a plot of the waning functions
 ui <- fluidPage(
-
     # Application title
     titlePanel("Vaccine-derived Immunity Waning by dose timing"),
-
+    
     # Sidebar with a slider input for time of peak first dose protection
     sidebarLayout(
         sidebarPanel(
-            sliderInput("peak",
-                        "Peak first dose immune timing:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
             sliderInput(
-               
+                "FirstPeak",
+                "Peak timing of response to First Dose",
+                min = 0,
+                max = 2,
+                value = 1
+            ),
+            sliderInput(
+                "FirstDuration",
+                "Duration of Immunity from First Dose",
+                min = 1,
+                max = 4,
+                value = 2
+                
+            ),
+            sliderInput(
+                "SecondPeak",
+                "Peak timing of second dose response",
+                min=1,
+                max=4,
+                value=1
+            ),
+            sliderInput(
+                "MaxFirst",
+                "Maximum immune response from first dose",
+                min = 1,
+                max = 2,
+                value = 1
+            ),
+            sliderInput(
+                "MaxSecond",
+                "Maximum immune response from second dose",
+                min = 2,
+                max = 4,
+                value = 2
             )
+            
         ),
-
+        
         # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+        mainPanel(plotOutput("wanePlot"))
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$peak + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$wanePlot <- renderPlot({
+        t <- seq(0, input$FirstDuration, 0.001)
+        Response1 <- -(t - input$FirstPeak) ^ 4 + input$MaxFirst
+        Response2 <- -(t-input$SecondPeak) ^ 4 + input$MaxSecond
+          wanePlot<-
     })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
